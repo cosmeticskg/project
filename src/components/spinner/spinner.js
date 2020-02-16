c020667798622217fc59b3f381758ccc92c77432
Sula <magina70@gmail.com>
2020-02-17 00:45:37 +0600

    add spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./spinner.css";
 
 const Spinner = () => {
   return (
-    <p>LOADING...</p>
-  )
+    <div className='spinner-wrapper'>
+      <div class="loadingio-spinner-wedges-u2xo70ixo1">
+        <div class="ldio-gjbndpoel69">
+          <div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
