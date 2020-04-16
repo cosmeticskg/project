e77cd18a61976eb507067aff7acec670b34c6a63
Sula <magina70@gmail.com>
2020-04-16 23:08:44 +0600

    add redux in production ver

--- src/index.js
+++ src/index.js
@@ -3,23 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
-// import 'primereact/resources/themes/nova-light/theme.css';
-// import 'primereact/resources/primereact.min.css';
-// import 'primeicons/primeicons.css';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./root-reducer";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+const composeEnhancers =
+  window.REDUX_DEVTOOLS_EXTENSION_COMPOSE ?
+    window.REDUX_DEVTOOLS_EXTENSION_COMPOSE({
+      // options like actionSanitizer, stateSanitizer
+    }) : compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
 );
-// store.getState();
-// console.log(store.getState());
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
