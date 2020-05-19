955d814c389537b79686436f449985b301238093
Sula <magina70@gmail.com>
2020-05-19 19:12:02 +0600

    add lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./app.css";
 import Home from "./containers/home-page";
-import Cart from "./containers/cart-page";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import FavoritePage from "./containers/favorite-page/favorite-page";
-import NotFound from "./components/404";
-import FilterPage from "./containers/filter-page";
-import StocksPage from "./containers/stocks-page";
-import Stocks from "./containers/stocks-page/stocks";
-import ProductInfoPage from "./containers/product-info";
+import ErrorBoundry from "./components/error-boundry";
+import Spinner from "./components/spinner/spinner";
+
+const ProductInfoPage = React.lazy(() => import("./containers/product-info"));
+const FavoritePage = React.lazy(() => import("./containers/favorite-page/favorite-page"));
+const NotFound = React.lazy(() => import("./components/404"));
+const FilterPage = React.lazy(() => import("./containers/filter-page"));
+const StocksPage = React.lazy(() => import("./containers/stocks-page"));
+const Stocks = React.lazy(() => import("./containers/stocks-page/stocks"));
+const Cart = React.lazy(() => import("./containers/cart-page"));
 
 function App() {
   return (
-    <Router>
-      <div className="main__wrapper">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/favorite" component={FavoritePage} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/filters" component={FilterPage} />
-          <Route path="/stocks" exact component={Stocks} />
-          <Route path="/stocks/:id" component={StocksPage} />
-          <Route path="/product/:productId" component={ProductInfoPage} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
+    <ErrorBoundry>
+      <Router>
+        <Suspense fallback={<Spinner />}>
+          <div className="main__wrapper">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/favorite" component={FavoritePage} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/filters" component={FilterPage} />
+              <Route path="/stocks" exact component={Stocks} />
+              <Route path="/stocks/:id" component={StocksPage} />
+              <Route path="/product/:productId" component={ProductInfoPage} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </Suspense>
+      </Router>
+    </ErrorBoundry>
   );
 }
 
